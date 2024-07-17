c233d5802559e170bbe4e096b0bc549c708ac11a
Prabhpreet Singh <prabhpreet694@gmail.com>
2024-07-17 15:41:03 +0530

    day 33

--- dp48.cpp
+++ dp48.cpp
@@ -47,3 +47,32 @@ int matrixMultiplication(vector<int> &nums, int N)
     vector<vector<int>>dp(N,vector<int>(N,-1));
         return findAns(1,N-1,nums,dp);
 }
+
+
+
+    // Tabulation
+
+    int matrixMultiplication(vector<int> &nums, int N)
+{
+    vector<vector<int>>dp(N,vector<int>(N,0));
+        // return findAns(1,N-1,nums,dp);
+
+        for(int i=1;i<N;i++){
+            dp[i][i]=0;
+        }
+
+        // before i was 1->n-1 now its opposite
+        // before j started from n-1 now opposite and j has to be greater than i so its i+1
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int mini=1e9;
+                for(int k=i;k<j;k++){
+                    int steps=nums[i-1]*nums[k]*nums[j]+dp[i][k]+dp[k+1][j];
+                    if(steps<mini)mini=steps;
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][N-1];
+
+}
